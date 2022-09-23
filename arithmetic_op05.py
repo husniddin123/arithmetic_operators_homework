8e8ee6096969d32cfa706e746e7cd440f6a9b836
husniddin <baxodirovhusniddin66@gemal.com>
2022-09-23 17:06:19 +0500

    buladimmi

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,7 +1,8 @@
 #Create a variable called 'number' with data type int.
- 
+number = 23 
 #Create a variable called 'answer'
 
 #Find the remainder of the division of number by 3.
- 
+ answer = number 2 % 0 
 #Print the answer
+print(answer)
