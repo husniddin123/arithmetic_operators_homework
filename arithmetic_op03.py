d50b86f4ea753716a5ced7e89d407b032dbc328d
husniddin <baxodirovhusniddin66@gemal.com>
2022-09-23 16:54:42 +0500

    manaaa

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,6 +1,9 @@
 #Create a variable called 'number' and assign it the two-digit number
-number = 45
+from ssl import SSL_ERROR_SSL
+
+
+number = 34
 #Find the reverse of the number and assign it to a variable called 'answer'.
-answer = number // 100  + number * 1
+answer = number // 10  + number % 10 * 10
 #Print the answer variable
 print(answer)
